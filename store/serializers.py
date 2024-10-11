346232641be785dcbf73d19c374ca982db948147
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-10-11 12:57:34 +0300

    add Product list, detail serializers

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from store.models import Topic, Form
+from store.models import Topic, Form, Product
 
 
 class TopicSerializer(serializers.ModelSerializer):
@@ -13,3 +13,34 @@ class FormSerializer(serializers.ModelSerializer):
     class Meta:
         model = Form
         fields = ("id", "name")
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            "id",
+            "name",
+            "image",
+            "size",
+            "material",
+            "color",
+            "description",
+            "tags",
+            "price",
+        )
+
+
+class ProductListSerializer(ProductSerializer):
+    class Meta:
+        model = Product
+        fields = ("name", "image", "size", "price")
+
+
+class ProductDetailSerializer(ProductSerializer):
+    topic = TopicSerializer(many=False, read_only=True)
+    form = FormSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ("name", "image", "size", "price", "topic", "form")
