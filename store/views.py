79b96fd72c9fc53d591b9210ea90a4cf3346c677
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-10-11 12:22:53 +0300

    add TopicViewSet

--- store/views.py
+++ store/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from store.models import Topic
+from store.permissions import IsAdminOrReadOnly
+from store.serializers import TopicSerializer
+
+
+class TopicViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    GenericViewSet
+):
+    queryset = Topic.objects.all()
+    serializer_class = TopicSerializer
+    permission_classes = (IsAdminOrReadOnly,)
