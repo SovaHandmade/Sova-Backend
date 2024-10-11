1cfbdf426afea291cef7b253bbf14a13798402a0
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-10-11 18:35:14 +0300

    add ProductViewSet

--- store/views.py
+++ store/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from store.models import Topic, Form
+from store.models import Topic, Form, Product
 from store.permissions import IsAdminOrReadOnly
-from store.serializers import TopicSerializer, FormSerializer
+from store.serializers import (
+    TopicSerializer,
+    FormSerializer,
+    ProductSerializer,
+    ProductListSerializer,
+    ProductDetailSerializer,
+    ProductImageSerializer,
+)
 
 
 class TopicViewSet(
@@ -31,3 +41,59 @@ class FormViewSet(
     queryset = Form.objects.all()
     serializer_class = FormSerializer
     permission_classes = (IsAdminOrReadOnly,)
+
+
+class ProductViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    GenericViewSet,
+):
+    queryset = Product.objects.prefetch_related("form", "topic")
+    serializer_class = ProductSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+    def get_queryset(self):
+        form = self.request.query_params.get("form")
+        topic = self.request.query_params.get("topic")
+
+        queryset = self.queryset
+
+        if form:
+            queryset = queryset.filter(form__icontains=form)
+
+        if topic:
+            queryset = queryset.filter(topic__icontains=topic)
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ProductListSerializer
+
+        if self.action == "retrieve":
+            return ProductDetailSerializer
+
+        if self.action == "upload_image":
+            return ProductImageSerializer
+
+        return ProductSerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        permission_classes=[IsAdminUser],
+    )
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading image to specific movie"""
+        product = self.get_object()
+        serializer = self.get_serializer(product, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
