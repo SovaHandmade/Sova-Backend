0e9b463a230689776955196eaad1737ee720a80d
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-10-12 16:51:57 +0300

    add paths to forms and topics

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from store.views import ProductViewSet
+from store.views import ProductViewSet, FormViewSet, TopicViewSet
 
 router = routers.DefaultRouter()
 router.register("product", ProductViewSet)
+router.register("form", FormViewSet)
+router.register("topic", TopicViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
