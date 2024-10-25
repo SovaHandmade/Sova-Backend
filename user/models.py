bbffc072fdada3a5eae18a2a5662ef26a587b98f
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-10-25 14:34:16 +0300

    add default User model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from .managers import UserManager
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(
+        "Email Address",
+        unique=True,
+    )
+
+    USERNAME_FIELD = "full_name"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.full_name
