18afffa9e9beda13aa801f58e6e51105706e3534
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-10-26 14:40:25 +0300

    change token_obtain_pair url

--- user/urls.py
+++ user/urls.py
@@ -8,7 +8,7 @@ from user.views import CreateUserView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token-renew/", TokenRefreshView.as_view(), name="token_refresh"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
