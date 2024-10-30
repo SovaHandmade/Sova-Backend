b6f861476f7a3582fb38d0876036e7d707301cf1
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-10-30 21:28:23 +0200

    add OrderListView

--- order/views.py
+++ order/views.py
@@ -1,3 +1,42 @@
+from django.db.models import Sum, ExpressionWrapper, F, IntegerField, Prefetch
 from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from order.models import Order, OrderItem
+from order.serializers import OrderSerializer
+
+
+class ReservationPagination(PageNumberPagination):
+    page_size = 10
+    max_page_size = 100
+
+
+class OrderListView(mixins.CreateModelMixin, mixins.ListModelMixin, GenericViewSet):
+    queryset = Order.objects.annotate(
+        total=Sum(
+            ExpressionWrapper(
+                F("product.price") * F("quantity"), output_field=IntegerField()
+            )
+        ),
+    ).prefetch_related(
+        Prefetch(
+            "items",
+            queryset=OrderItem.objects.annotate(
+                total=ExpressionWrapper(
+                    F("product.price") * F("quantity"), output_field=IntegerField()
+                )
+            ),
+        )
+    )
+    serializer_class = OrderSerializer
+    pagination_class = ReservationPagination
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
