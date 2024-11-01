c5fe16e5afd488dae0d20663928948350f2db7a6
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-11-01 19:21:14 +0200

    add settings for reset password functionality

--- SovaStore/settings.py
+++ SovaStore/settings.py
@@ -154,3 +154,12 @@ SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("JWT",),
     "AUTH_HEADER_NAME": "HTTP_AUTHORIZE",
 }
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "sovastorehelper@gmail.com"
+EMAIL_HOST_PASSWORD = "stsjkmbukhlvnfmd"
+DEFAULT_FROM_EMAIL = "sovastorehelper@gmail.com"
