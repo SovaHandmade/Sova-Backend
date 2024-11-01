65cbdb952232a32dbfe74ad5333615205df2248c
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-11-01 20:13:51 +0200

    rewrite OrderViewSet

--- order/views.py
+++ order/views.py
@@ -6,13 +6,25 @@ from order.models import Order
 from order.serializers import OrderSerializer
 
 
-class OrderViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, GenericViewSet):
-    queryset = Order.objects.select_related("items")
+class OrderViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericViewSet,
+):
+    queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = (IsAuthenticated,)
 
-    def get_queryset(self):
-        return Order.objects.filter(user=self.request.user)
+    def filter_queryset(self, queryset):
+        queryset = super().filter_queryset(queryset)
+        user = self.request.user
+
+        if user.is_staff:
+            return queryset
+
+        return queryset.filter(user=user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
