1448d5a2cfbde86a3074443f0825528801a51343
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-11-02 12:40:51 +0200

    add urls for OrderItemViewSet

--- order/urls.py
+++ order/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from order.views import OrderViewSet
+from order.views import OrderViewSet, OrderItemViewSet
 
 router = routers.DefaultRouter()
 router.register("", OrderViewSet)
+router.register("order-item", OrderItemViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
