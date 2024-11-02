52afc2c374aee94a54d536cf3d293129f15c9327
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-11-02 16:07:57 +0200

    rewrite CorsMiddleware

--- middleware.py
+++ middleware.py
@@ -1,4 +1,5 @@
 from django import http
+from django.conf import settings
 
 
 class CorsMiddleware(object):
@@ -7,19 +8,24 @@ class CorsMiddleware(object):
 
     def __call__(self, request):
         response = self.get_response(request)
-        if (
-            request.method == "OPTIONS"
-            and "HTTP_ACCESS_CONTROL_REQUEST_METHOD" in request.META
-        ):
+
+        allowed_origins = getattr(settings, "CORS_ALLOWED_ORIGINS", [])
+
+        origin = request.META.get("HTTP_ORIGIN")
+        if origin in allowed_origins:
+            response["Access-Control-Allow-Origin"] = origin
+        elif "*" in allowed_origins:
+            response["Access-Control-Allow-Origin"] = "*"
+
+        if request.method == "OPTIONS" and "HTTP_ACCESS_CONTROL_REQUEST_METHOD" in request.META:
             response = http.HttpResponse()
             response["Content-Length"] = "0"
-            response["Access-Control-Max-Age"] = 86400
-        response["Access-Control-Allow-Origin"] = "*"
-        response["Access-Control-Allow-Methods"] = (
-            "DELETE, GET, OPTIONS, PATCH, POST, PUT"
-        )
+            response["Access-Control-Max-Age"] = "86400"
+
+        response["Access-Control-Allow-Methods"] = "DELETE, GET, OPTIONS, PATCH, POST, PUT"
         response["Access-Control-Allow-Headers"] = (
             "accept, accept-encoding, authorization, content-type, dnt, origin, user-agent, x-csrftoken, "
             "x-requested-with"
         )
+
         return response
