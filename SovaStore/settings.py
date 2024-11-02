83f82473947fec042ca75627fa98735542b7013f
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-11-02 16:08:33 +0200

    delete unused middleware

--- SovaStore/settings.py
+++ SovaStore/settings.py
@@ -60,7 +60,6 @@ MIDDLEWARE = [
     "middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
--- middleware.py
+++ middleware.py
@@ -17,12 +17,17 @@ class CorsMiddleware(object):
         elif "*" in allowed_origins:
             response["Access-Control-Allow-Origin"] = "*"
 
-        if request.method == "OPTIONS" and "HTTP_ACCESS_CONTROL_REQUEST_METHOD" in request.META:
+        if (
+            request.method == "OPTIONS"
+            and "HTTP_ACCESS_CONTROL_REQUEST_METHOD" in request.META
+        ):
             response = http.HttpResponse()
             response["Content-Length"] = "0"
             response["Access-Control-Max-Age"] = "86400"
 
-        response["Access-Control-Allow-Methods"] = "DELETE, GET, OPTIONS, PATCH, POST, PUT"
+        response["Access-Control-Allow-Methods"] = (
+            "DELETE, GET, OPTIONS, PATCH, POST, PUT"
+        )
         response["Access-Control-Allow-Headers"] = (
             "accept, accept-encoding, authorization, content-type, dnt, origin, user-agent, x-csrftoken, "
             "x-requested-with"
