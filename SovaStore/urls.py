9d39ae3be461399560bfc749b01ac22ff35fe250
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-11-02 11:54:53 +0200

    rename urls for documentation

--- SovaStore/urls.py
+++ SovaStore/urls.py
@@ -49,14 +49,14 @@ urlpatterns = [
         auth_views.PasswordResetCompleteView.as_view(),
         name="password_reset_complete",
     ),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/schema/swagger-ui/",
+        "schema/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
     path(
-        "api/schema/redoc/",
+        "schema/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
