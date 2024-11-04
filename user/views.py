f3bfe2582a79d8ec10b5af2e4d09857cd117885a
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-11-04 21:24:57 +0200

    add UserViewSet

--- user/views.py
+++ user/views.py
@@ -1,5 +1,7 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
+from rest_framework import generics, viewsets, mixins
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.viewsets import GenericViewSet
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from user.serializers import UserSerializer
@@ -16,3 +18,9 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class UserViewSet(mixins.RetrieveModelMixin, GenericViewSet):
+    serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
+    queryset = get_user_model().objects.all()
