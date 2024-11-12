d6370501a6ccc61859ee25f7a37b53ed6f0c218b
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-11-12 22:26:34 +0200

    delete whitenoise

--- SovaStore/settings.py
+++ SovaStore/settings.py
@@ -58,7 +58,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -140,7 +139,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = "staticfiles/"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = "uploads/media/"
